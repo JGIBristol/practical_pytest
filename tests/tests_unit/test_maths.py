b8345e407403f24040566a1f0192da049f1d4218
Richard Lane <richard.lane@bristol.ac.uk>
2025-06-05 11:29:24 +0100

    uv stuff

---
+++ tests/tests_unit/test_maths.py
@@ -0,0 +1,50 @@
+"""
+Tests for the maths module
+"""
+import numpy as np
+
+from practical_pytest import maths
+
+
+def test_mean():
+    """
+    Check that we can find the mean of some complex numbers
+    """
+    numbers = [2.0 + 2.0j, 3.0 + 3.0j, 4.0 + 1.0j]
+
+    expected_mean = 3.0 + 2.0j
+    calculated_mean = maths.mean(numbers)
+
+    assert np.isclose(calculated_mean, expected_mean)
+
+
+def test_mean_floats():
+    """
+    Check that we can find the mean of some floats
+    """
+    numbers = [1.0, 2.5, 3.5]
+
+    expected_mean = 7 / 3
+    calculated_mean = maths.mean(numbers)
+
+    assert np.isclose(expected_mean, calculated_mean)
+
+
+def test_mean_negative():
+    """
+    Check that we can find the mean of some negative floats
+    """
+    numbers = [-1.0, -5.0, -3.5]
+
+    expected_mean = -9.5 / 3
+    calculated_mean = maths.mean(numbers)
+
+    assert np.isclose(expected_mean, calculated_mean)
+
+
+def test_mean_empty():
+    """
+    Maybe for my purposes, I expect the mean of an empty list to evaluate to 0
+
+    """
+    assert maths.mean([]) == 0.0
